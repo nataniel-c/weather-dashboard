5b2b528fd76a91b4e71b40032ad6eb8f0b0c78ac
Nataniel <nataniel-carras@hotmail.com>
2023-09-18 10:19:31 -0500

    added to html and js

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Weather Dashboard
 
 ## Description
-This code is used to 
+This code is used to create a weather dashboard with form inputs that allow the user to search for a city and recieve the present and future weather conditions for that city.
+
+## Website Link
+https://nataniel-c.github.io/weather-dashboard
+
+## Website Screenshots
+![screenshot1](./assets/images/screenshot1.PNG)
+---
+
 AS A traveler
 I WANT to see the weather outlook for multiple cities
 SO THAT I can plan a trip accordingly
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,79 @@
+var searchInputEl = document.querySelector('#search-input');
 var searchFormEl = document.querySelector('#search-form');
 
-function handleSearchFormSubmit(event) {
+
+var handleSearchFormSubmit = function (event) {
   event.preventDefault();
 
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
+  var searchInput = searchInputEl.value.trim();
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
+  if (!searchInput) {
+    console.error('Please enter the name of a city');
     return;
+  } else {
+    getCoordinates(searchInput);
   }
-
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-  location.assign(queryString);
 }
+
+var getCoordinates = function (location) {
+
+  var geoApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='
+  + location +
+  + '&appid=&appid=36e56fdbc7dc57b349ba31ffef417a61';
+  
+  fetch(geoApiUrl)
+    .then(function (response) {
+      getForecast(response.lat, response.lon);
+    })
+    .catch(function (error) {
+      alert('Unable to connect to OpenWeather');
+    });
+  
+}
+
+var getForecast = function (latitude, longitude) {
+  var weatherApiUrl = 'api.openweathermap.org/data/2.5/forecast?lat=' 
+  + latitude 
+  + '&' + 'lon=' 
+  + longitude
+  + '&units=standard&cnt=5&appid=36e56fdbc7dc57b349ba31ffef417a61';
+
+  fetch(weatherApiUrl)
+    .then(function (response) {
+      loadForecast(response);
+    })
+    .catch(function (error) {
+      alert('Unable to connect to GitHub');
+    });
+};
+
+function loadForecast(forecast) {
+  // for the 5 cards that should be in the 5 day forecast
+  // Make a card that has the day
+
+  for (var i = 0; i < 6; i++) {
+    var issueEl = document.createElement('a');
+    issueEl.classList = 'list-item flex-row justify-space-between align-center';
+    issueEl.setAttribute('href', issues[i].html_url);
+    issueEl.setAttribute('target', '_blank');
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = issues[i].title;
+    issueEl.appendChild(titleEl);
+
+    var typeEl = document.createElement('span');
+
+    if (issues[i].pull_request) {
+      typeEl.textContent = '(Pull request)';
+    } else {
+      typeEl.textContent = '(Issue)';
+    }
+
+    issueEl.appendChild(typeEl);
+
+    issueContainerEl.appendChild(issueEl);
+  }
+}
+
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
